bbcd1a3929d8c4076c5b3907991136fd8bb6d223
h8society <ivorchalov@gmail.com>
2025-03-31 19:54:43 +0300

    Initial commit

---
+++ bank-emulator/src/main/java/ru/vorchalov/bank_emulator/BankEmulatorApplication.java
@@ -0,0 +1,13 @@
+package ru.vorchalov.bank_emulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankEmulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankEmulatorApplication.class, args);
+	}
+
+}
---
+++ bank-emulator/src/test/java/ru/vorchalov/bank_emulator/BankEmulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vorchalov.bank_emulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankEmulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
